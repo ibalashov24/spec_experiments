76c578b8b5f16c6c53714f60379a2d6cbeb91ac3
Ilya Balashov <ibalashov24@gmail.com>
2020-08-17 06:08:00 +0300

    Implement multiplication of sparse matrix by vector

--- launcher.c
+++ launcher.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <inttypes.h>
 #include <stdint.h>
 
 struct CooElement {
-	uint32_t row, col;
+	int32_t row, col;
 	double value;
+};
+
+struct CooVectorElement {
+    int32_t pos;
+    double value;
 };
 
 void mult_matrix_vector(
@@ -11,9 +17,17 @@ void mult_matrix_vector(
 		int32_t matrix_rows,
 		int32_t matrix_cols,
 		int32_t nonzero_count,
-		double *vector,
-		int32_t vector_size,
-		double *result); 
+		const struct CooVectorElement *vector,
+		int32_t vector_nonzero_count,
+        struct CooVectorElement *buffer,
+		struct CooVectorElement *result,
+        int32_t *result_size); 
+
+
+void println(int32_t val)
+{
+    printf("%" PRId32 "\n", val);
+}
 
 int main()
 {
@@ -22,19 +36,18 @@ int main()
 	coo[1] = (struct CooElement) { 0, 1, 1.0 };
 	coo[2] = (struct CooElement) { 1, 1, 2.0 };
 
+    struct CooVectorElement vector[1];
+    vector[0] = (struct CooVectorElement) { 1, 5.0 };
 
-//	printf("%d %d %f", coo[1].row, coo[1].col, coo[1].value);
+    struct CooVectorElement buffer[8];	
+    struct CooVectorElement result[8];
+    int32_t result_size;
+	mult_matrix_vector(coo, 2, 2, 3, vector, 1, buffer, result, &result_size);	
 
-	double vector[2];
-	vector[0] = -1.0;
-	vector[1] = 0.5;
-
-	double result[2];
-	
-	mult_matrix_vector(coo, 2, 2, 3, vector, 2, result);	
-
-	for (int i = 0; i < 2; ++i)
-		printf("%f\n", result[i]);
+    printf("Result size: %" PRId32 "\n", result_size);
+    printf("Result vector: \n");
+	for (int i = 0; i < result_size; ++i)
+		printf("%d %f\n", result[i].pos, result[i].value);
 
 	return 0;
 }
