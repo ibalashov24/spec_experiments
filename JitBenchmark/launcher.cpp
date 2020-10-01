19a68b5bc7de8f345522f03ca62e803351a3f67c
Ilya Balashov <ibalashov24@gmail.com>
2020-10-01 15:33:43 +0300

    Fix JIT benchmark build problems

--- JitBenchmark/launcher.cpp
+++ JitBenchmark/launcher.cpp
@@ -7,7 +7,7 @@
 #include <benchmark/benchmark.h>
 
 #define RUNTIME_ENABLE_JIT
-#include <anydsl_runtime.h>
+#include <anydsl_runtime.hpp>
 
 struct CooElement {
 	int32_t row, col;
@@ -43,15 +43,17 @@ std::string generate_function_matrix(
         const std::vector<CooElement> &matrix,
         int32_t matrix_size)
 {
-    std::string result = "fn @second(i : i32) -> CooElement { \
-                          match i {\
-                              ";
+    std::string result = "fn @second(i : i32) -> CooElement { \n\
+                          match i {\n";
     for (int i = 0; i < matrix_size; ++i)
     {
-        result += std::to_string(i) + " => " + std::to_string(matrix[i].value) + ",";
+        result += std::to_string(i) + " => CooElement {row : " + 
+            std::to_string(matrix[i].row) + 
+            ", col : " + std::to_string(matrix[i].col) +
+            ", value : " + std::to_string(matrix[i].value) + "},\n";
     }
-
-    result += "}; }";
+    result += "_ => CooElement {row : 0, col : 0, value : 1.000000}\n";
+    result += "} }\n";
 
     return result;
 }
@@ -62,81 +64,73 @@ exec_fn compile_spec_function(
         const std::vector<CooElement> &matrix,
         int32_t matrix_size)
 {
-    std::string program_string =
-" struct CooElement {\
-       row : i32,\
-       col : i32,\
-       value : f64\
-}" + generate_function_matrix(matrix, matrix_size) +
+    std::string program_string = " \
+ struct CooElement {\n\
+       row : i32,\n\
+       col : i32,\n\
+       value : f64\n\
+}\n" + generate_function_matrix(matrix, matrix_size) +
 
-"fn @(?a & ?b & ?s) unroll_step(a : i32, @b : i32, @s : i32, f : fn(i32) -> ()) -> () {\
-    if a < b { \
-        @@f(a); \
-        unroll_step(a + s, b, s, f) \
-    }\
-}\
-fn @unroll(a : i32, b : i32, f : fn(i32) -> ()) -> () { \
-    unroll_step(a, b, 1, f) \
-}\
-\
-// Calculates the product of two sparse matrices\
-// Both in COO format\
-extern\
-fn @mult_matrix_matrix(\
-        first_matrix: &[CooElement],\
-        first_matrix_nonzero: i32,\
-        second_matrix: fn(i32) -> f64,\
-        second_matrix_nonzero : i32,\
-        buffer: &mut [CooElement],\
-        result: &mut [CooElement]) -> () {\
-\
-    let mut current_buffer_pos : i32 = 0; \
-    for i in unroll(0, first_matrix_nonzero) {\
-        for j in unroll(0, second_matrix_nonzero) {\
-            if second_matrix(j).row == first_matrix(i).col {\
-                let new_value : f64 = second_matrix(j).value * first_matrix(i).value;\
-                buffer(current_buffer_pos).value = new_value;\
-\
-        \
-                buffer(current_buffer_pos).row = first_matrix(i).row;\
-                buffer(current_buffer_pos).col = second_matrix(j).col;\
-                current_buffer_pos += 1\
-            }\
-        }\
-    }\
-\
-    let mut current_result_pos : i32 = 0;\
-    for i in unroll(0, current_buffer_pos) {\
-        for j in unroll(0, current_result_pos + 1) {\
-            if j == current_result_pos {\
-                result(current_result_pos) = buffer(i);\
-                current_result_pos += 1;\
-                break()\
-            } else if result(j).row == buffer(i).row && result(j).col == buffer(i).col {\
-                result(j).value += buffer(i).value\
-            }\
-        }\
-    }\
-\
-    *result_size = current_result_pos \
-}\
-\
-fn @wrapper( \
-        first : $[CooElement], first_count : i32, \
-        buffer: &mut [CooElement],\
-        result: &mut [CooElement]) -> () \
-{\
+"\n\
+// Calculates the product of two sparse matrices\n\
+// Both in COO format\n\
+extern\n\
+fn @mult_matrix_matrix(\n\
+        first_matrix: &[CooElement],\n\
+        first_matrix_nonzero: i32,\n\
+        second_matrix: fn(i32) -> CooElement,\n\
+        second_matrix_nonzero : i32,\n\
+        buffer: &mut [CooElement],\n\
+        result: &mut [CooElement]) -> () {\n\
+\n\
+    let mut current_buffer_pos : i32 = 0; \n\
+    for i in unroll(0, first_matrix_nonzero) {\n\
+        for j in unroll(0, second_matrix_nonzero) {\n\
+            if second_matrix(j).row == first_matrix(i).col {\n\
+                let new_value : f64 = second_matrix(j).value * first_matrix(i).value;\n\
+                buffer(current_buffer_pos).value = new_value;\n\
+\n\
+        \n\
+                buffer(current_buffer_pos).row = first_matrix(i).row;\n\
+                buffer(current_buffer_pos).col = second_matrix(j).col;\n\
+                current_buffer_pos += 1\n\
+            }\n\
+        }\n\
+    }\n\
+\n\
+    let mut current_result_pos : i32 = 0;\n\
+    for i in unroll(0, current_buffer_pos) {\n\
+        for j in unroll(0, current_result_pos + 1) {\n\
+            if j == current_result_pos {\n\
+                result(current_result_pos) = buffer(i);\n\
+                current_result_pos += 1;\n\
+                break()\n\
+            } else if result(j).row == buffer(i).row && result(j).col == buffer(i).col {\n\
+                result(j).value += buffer(i).value\n\
+            }\n\
+        }\n\
+    }\n\
+\n\
+}\n\
+\n\
+extern \
+fn wrapper( \n\
+        first : &[CooElement], first_count : i32, \n\
+        buffer: &mut [CooElement],\n\
+        result: &mut [CooElement]) -> () \n\
+{\n\
     mult_matrix_matrix(first, first_count, second, " + std::to_string(matrix_size)
-    + "buffer, result, result_size);\
-}\
-"; 
+    + ", buffer, result);\n\
+}\n\
+";
     auto key = anydsl_compile(
             program_string.c_str(), 
             (uint32_t) program_string.size(),
-            3);
+            2);
 
     if (auto ptr = anydsl_lookup_function(key, "wrapper")) {
         auto fun = reinterpret_cast<exec_fn>(ptr);
+        return fun;
     } else {
         std::cout << "Compilation failed!" << std::endl;
     }
@@ -164,25 +158,23 @@ static void BM_mult_bcsstk16_2blocks(benchmark::State &state)
 }
 BENCHMARK(BM_mult_bcsstk16_2blocks);
 
-/*static void BM_mult_fs1831_2blocks(benchmark::State &state)
+static void BM_mult_fs1831_2blocks(benchmark::State &state)
 {
     auto left_matrix = read_matrix("Matrix/fs_183_1"); // Size 1069 
     auto right_matrix = read_matrix("Matrix/2blocks"); // Size 8
 
     auto buffer = new CooElement[10000];
     auto result = new CooElement[10000];
-    auto result_size = new int;
     
+    auto wrapper = compile_spec_function(right_matrix, 8);
+
     for (auto _ : state)
     {
-        mult_matrix_matrix(left_matrix.data(), 1069,
-                           right_matrix.data(), 8,
-                           buffer, result, result_size); 
-    } 
+        wrapper(left_matrix.data(), 1069, buffer, result);
+    }
 
     delete [] buffer;
     delete [] result;
-    delete result_size;
 }
 BENCHMARK(BM_mult_fs1831_2blocks);
 
@@ -193,18 +185,16 @@ static void BM_mult_bccsstk16_eye3(benchmark::State &state)
 
     auto buffer = new CooElement[2000000];
     auto result = new CooElement[2000000];
-    auto result_size = new int;
-    
+ 
+    auto wrapper = compile_spec_function(right_matrix, 3);
+
     for (auto _ : state)
     {
-        mult_matrix_matrix(left_matrix.data(), 147631,
-                           right_matrix.data(), 3,
-                           buffer, result, result_size); 
-    } 
+        wrapper(left_matrix.data(), 147631, buffer, result);
+    }
 
     delete [] buffer;
     delete [] result;
-    delete result_size;
 }
 BENCHMARK(BM_mult_bccsstk16_eye3);
 
@@ -215,20 +205,18 @@ static void BM_mult_fs1832_eye3(benchmark::State &state)
 
     auto buffer = new CooElement[4000];
     auto result = new CooElement[4000];
-    auto result_size = new int;
+    
+    auto wrapper = compile_spec_function(right_matrix, 3);
     
     for (auto _ : state)
     {
-        mult_matrix_matrix(left_matrix.data(), 1069,
-                           right_matrix.data(), 3,
-                           buffer, result, result_size); 
+        wrapper(left_matrix.data(), 1069, buffer, result);
     } 
 
     delete [] buffer;
     delete [] result;
-    delete result_size;
 }
-BENCHMARK(BM_mult_bcsstk16_2blocks);*/
+BENCHMARK(BM_mult_bcsstk16_2blocks);
 
 
 BENCHMARK_MAIN();
