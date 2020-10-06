8c5e0ba8ce1b9283b52cb656fe8aca7580f983c0
Ilya Balashov <ibalashov24@gmail.com>
2020-10-06 16:35:13 +0300

    Remove buffer

--- JitBenchmark/spec_all.cpp
+++ JitBenchmark/spec_all.cpp
@@ -80,7 +80,6 @@ fn mult_matrix_matrix(\n\
         first_matrix_nonzero: i32,\n\
         second_matrix: fn(i32) -> CooElement,\n\
         second_matrix_nonzero : i32,\n\
-        buffer: &mut [CooElement],\n\
         result: &mut [CooElement]) -> () {\n\
 \n\
     let mut current_buffer_pos : i32 = 0; \n\
@@ -88,39 +87,24 @@ fn mult_matrix_matrix(\n\
         for j in unroll(0, second_matrix_nonzero) {\n\
             if second_matrix(j).row == first_matrix(i).col {\n\
                 let new_value : f64 = second_matrix(j).value * first_matrix(i).value;\n\
-                buffer(current_buffer_pos).value = new_value;\n\
-\n\
-        \n\
-                buffer(current_buffer_pos).row = first_matrix(i).row;\n\
-                buffer(current_buffer_pos).col = second_matrix(j).col;\n\
-                current_buffer_pos += 1\n\
-            }\n\
-        }\n\
-    }\n\
-\n\
-    let mut current_result_pos : i32 = 0;\n\
-    for i in unroll(0, current_buffer_pos) {\n\
-        for j in unroll(0, current_result_pos + 1) {\n\
-            if j == current_result_pos {\n\
-                result(current_result_pos) = buffer(i);\n\
-                current_result_pos += 1;\n\
-                break()\n\
-            } else if result(j).row == buffer(i).row && result(j).col == buffer(i).col {\n\
-                result(j).value += buffer(i).value\n\
-            }\n\
-        }\n\
-    }\n\
-\n\
+                for k in unroll(0, current_result_pos + 1) {\n\
+                    if k == current_result_pos {\n\
+                        result(current_result_pos) = new_value;\n\
+                        current_result_pos += 1;\n\
+                        break()\n\
+                    } else if result(k).row == first_matrix(i).row && result(k).col == second_matrix(j).row {\n\
+                        result(j).value += new_value\n\
+                    }\n\
+                }\n\
 }\n\
 \n\
 extern \
 fn wrapper( \n\
         first : &[CooElement], first_count : i32, \n\
-        buffer: &mut [CooElement],\n\
         result: &mut [CooElement]) -> () \n\
 {\n\
     mult_matrix_matrix(first, first_count, second, " + std::to_string(matrix_size)
-    + ", buffer, result);\n\
+    + ", result);\n\
 }\n\
 ";
     auto key = anydsl_compile(
@@ -150,7 +134,7 @@ static void BM_mult_bcsstk16_2blocks(benchmark::State &state)
 
     for (auto _ : state)
     {
-        wrapper(left_matrix.data(), 147631, buffer, result);
+        wrapper(left_matrix.data(), 147631, result);
     } 
 
     delete [] buffer;
@@ -170,7 +154,7 @@ static void BM_mult_fs1831_2blocks(benchmark::State &state)
 
     for (auto _ : state)
     {
-        wrapper(left_matrix.data(), 1069, buffer, result);
+        wrapper(left_matrix.data(), 1069, result);
     }
 
     delete [] buffer;
@@ -190,7 +174,7 @@ static void BM_mult_bccsstk16_eye3(benchmark::State &state)
 
     for (auto _ : state)
     {
-        wrapper(left_matrix.data(), 147631, buffer, result);
+        wrapper(left_matrix.data(), 147631, result);
     }
 
     delete [] buffer;
@@ -210,7 +194,7 @@ static void BM_mult_fs1832_eye3(benchmark::State &state)
     
     for (auto _ : state)
     {
-        wrapper(left_matrix.data(), 1069, buffer, result);
+        wrapper(left_matrix.data(), 1069, result);
     } 
 
     delete [] buffer;
